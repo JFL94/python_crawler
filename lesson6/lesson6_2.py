1a5414cee5e2ba4039437d44ab26eed959f7a455
JFL94 <94jingfang@gmail.com>
2025-12-06 16:28:06 +0800

    modify

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -6,9 +6,17 @@ async def main():
     async with AsyncWebCrawler() as crawler:
         #Run the crawler on a URL
         result = await crawler.arun(url='https://www.bnext.com.tw/')
-
+        print(type(result))
         #列印取出的結果
-        print(result.markdown)
+        #print(result.markdown)
+        #print(result.cleaned_html)
+        #print(result.raw_html)
+        if result.success:            
+            with open("lesson6/lesson6_2_output.md", "w", encoding="utf-8") as f:
+                f.write(result.markdown)
+            print("存檔成功!")
+        else:
+            print("失敗。")
 
 #py檔執行
 asyncio.run(main())
